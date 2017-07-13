ff6633ecd9cb2405209e16be6d4d805b6cd7347d
Wei Quan <quan.we@gmail.com>
2017-07-13 15:40:44 -0400

    Fix javascript and dropdown menu

--- theme/javascripts/app.js
+++ theme/javascripts/app.js
@@ -6,24 +6,10 @@
         $(document).tooltips();
         $('input, textarea').placeholder();
 
-
-
         $(document).foundationButtons();
-
-
-
         $(document).foundationNavigation();
-
-
-
         $(document).foundationCustomForms();
-
-
-
-
-        $(document).foundationTabs({callback: $.foundation.customForms.appendCustomMarkup});
-
-
+        $(document).foundationTabs({ callback: $.foundation.customForms.appendCustomMarkup });
 
         // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
         // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
@@ -41,45 +27,38 @@
 
 })(jQuery);
 
-
-
 $(document).ready(function () {
 
-    < !-- data table -- >
-            $('.dataTable').dataTable(
-            {
-                "oLanguage": {
-                    "sLengthMenu": '<select >' +
-                            '<option value="10">10</option>' +
-                            '<option value="20">20</option>' +
-                            '<option value="30">30</option>' +
-                            '<option value="40">40</option>' +
-                            '<option value="50">50</option>' +
-                            '<option value="100">100</option>' +
-                            '<option value="-1">All</option>' +
-                            '</select>'
-                }
-            });
+    //< !-- data table -- >
+    $('.dataTable').dataTable(
+        {
+            "oLanguage": {
+                "sLengthMenu": '<select >' +
+                '<option value="10">10</option>' +
+                '<option value="20">20</option>' +
+                '<option value="30">30</option>' +
+                '<option value="40">40</option>' +
+                '<option value="50">50</option>' +
+                '<option value="100">100</option>' +
+                '<option value="-1">All</option>' +
+                '</select>'
+            }
+        });
 
-
-    < !-- number and money format -- >
-            $(".just_number").keypress(function (e)
-    {
+    //< !-- number and money format -- >
+    $(".just_number").keypress(function (e) {
         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
             return false;
         }
     });
 
-    $(".just_money").keypress(function (e)
-    {
+    $(".just_money").keypress(function (e) {
         if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
             return false;
         }
     });
-            < !-- /number and money format -->
+    //< !-- /number and money format -->
 
-
-            < !-- validate -- >
-            $("form").validate();
+    //< !-- validate -- >
+    $("form").validate();
 });
-  
